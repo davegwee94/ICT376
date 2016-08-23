6c61c80f7ea38682e7cb2b5d3a2a7b0a84f8e38c
fjrclean <finnian_ryan@iinet.net.au>
2016-08-23 17:34:43 +0800

    Created new android project using 4.2 replicant sdk with ant.

---
+++ src/edu/murdoch/ict376/team3/MainActivity.java
@@ -0,0 +1,15 @@
+package edu.murdoch.ict376.team3;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
